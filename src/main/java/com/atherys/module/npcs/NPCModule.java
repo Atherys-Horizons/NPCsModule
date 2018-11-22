6d3b882e589b528f660b0bb6319e8900240c4e3a
Liam <liamreadsspam@gmail.com>
2018-11-21 20:47:29 -0400

    Add load message

--- src/main/java/com/atherys/module/npcs/NPCModule.java
+++ src/main/java/com/atherys/module/npcs/NPCModule.java
@@ -7,6 +7,9 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
 
+import javax.inject.Inject;
+import java.util.logging.Logger;
+
 @Plugin(
         id = NPCModule.ID,
         name = NPCModule.NAME,
@@ -23,8 +26,12 @@ public class NPCModule {
     public static final String DESCRIPTION = "A plugin written for the A'therys Horizons server.";
     public static final String VERSION = "1.0.0";
 
+    @Inject
+    private Logger logger;
+
     private void start() {
         JavaScriptLibrary.getInstance().extendWith(NPCsExtension.getInstance());
+        logger.info("NPC Scripting module loaded!");
     }
 
     @Listener
