d53180148b0611b44f8a435790bf0898dab3e09d
Rynelf <liamreadsspam@gmail.com>
2019-01-26 19:39:55 -0400

    Create registry and reload NPCs plugin

--- src/main/java/com/atherys/module/npcs/NPCModule.java
+++ src/main/java/com/atherys/module/npcs/NPCModule.java
@@ -4,6 +4,7 @@ import com.atherys.module.npcs.event.RegisterNPCsEvent;
 import com.atherys.module.npcs.script.NPCsExtension;
 import com.atherys.script.js.JavaScriptLibrary;
 import me.mrdaniel.npcs.NPCs;
+import me.mrdaniel.npcs.io.NPCFile;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
@@ -11,8 +12,10 @@ import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
 
 import javax.inject.Inject;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Plugin(
@@ -33,9 +36,13 @@ public class NPCModule {
     public static final String VERSION = "1.0.0";
 
     private static boolean init;
+    private static Map<String, NPCFile> registry;
 
     @Inject
     private Logger logger;
+
+    @Inject
+    private PluginContainer container;
 
     private void init() {
         JavaScriptLibrary.getInstance().extendWith(NPCsExtension.getInstance());
@@ -46,6 +53,7 @@ public class NPCModule {
 
     private void start() {
         Sponge.getEventManager().post(new RegisterNPCsEvent(NPCs.getNPCManager()));
+        NPCs.reload(container);
     }
 
     @Listener
@@ -56,6 +64,14 @@ public class NPCModule {
     @Listener(order = Order.LATE)
     public void onStart(GameStartedServerEvent event) {
         if (init) start();
+    }
+
+    public static void registerNpc(String id, NPCFile npc) {
+        registry.put(id, npc);
+    }
+
+    public static NPCFile getNpc(String id) {
+        return registry.get(id);
     }
 
 }
--- src/main/java/com/atherys/module/npcs/script/CreateNPC.java
+++ src/main/java/com/atherys/module/npcs/script/CreateNPC.java
@@ -1,5 +1,7 @@
 package com.atherys.module.npcs.script;
 
+import com.atherys.module.npcs.NPCModule;
+import com.atherys.script.api.util.TriFunction;
 import me.mrdaniel.npcs.NPCs;
 import me.mrdaniel.npcs.exceptions.NPCException;
 import me.mrdaniel.npcs.io.NPCFile;
@@ -14,21 +16,22 @@ import java.util.function.BiFunction;
 /**
  * @jsfunc
  */
-public class CreateNPC implements BiFunction<String, Location<World>, NPCFile> {
+public class CreateNPC implements TriFunction<String, String, Location<World>, NPCFile> {
 
     /**
      * Creates an NPC of the given type at the given location.
      */
     @Override
-    public NPCFile apply(String entityTypeId, Location<World> location) {
+    public NPCFile apply(String entityTypeId, String id, Location<World> location) {
         Optional<EntityType> type = Sponge.getRegistry().getType(EntityType.class, entityTypeId);
         return type.map(entityType-> {
             try {
                 NPCFile npc = NPCs.getNPCManager().create(entityType, location);
                 npc.setTemporary(true);
+                NPCModule.registerNpc(id, npc);
                 return npc;
             } catch (NPCException e) {
-                NPCs.getInstance().getLogger().error(e.toString());
+                NPCs.getLogger().error(e.toString());
                 return null;
             }
         }).orElse(null);
