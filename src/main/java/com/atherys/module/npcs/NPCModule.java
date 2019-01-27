aa99384a77f67055d38b629357eb5569ac80be31
Liam <liamreadsspam@gmail.com>
2019-01-26 20:32:24 -0400

    Actually initialize the registry

--- src/main/java/com/atherys/module/npcs/NPCModule.java
+++ src/main/java/com/atherys/module/npcs/NPCModule.java
@@ -15,6 +15,7 @@ import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
 
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -36,7 +37,7 @@ public class NPCModule {
     public static final String VERSION = "1.0.0";
 
     private static boolean init;
-    private static Map<String, NPCFile> registry;
+    private static Map<String, NPCFile> registry = new HashMap<>();
 
     @Inject
     private Logger logger;
